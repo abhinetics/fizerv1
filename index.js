ca53ad705bf780ffb3170c83956c10d4f7758927
abhijeetkumarsinghofficial <hackker4200@gmail.com>
2024-06-16 11:24:06 +0530

    files

---
+++ index.js
@@ -0,0 +1,20 @@
+function Butclicked(){
+    console.log("clicked")
+    if(document.getElementById("username").value == "abhijeet2024@szoftvertelemechanics.com" && document.getElementById("password").value == "Ab@32234"){
+        window.location.href = "c6sj39db2sk24.html";
+        console.log("success")
+    }
+    else if(document.getElementById("username").value == "faishal2024@szoftvertelemechanics.com" && document.getElementById("password").value == "Fa@54677"){
+        window.location.href = "f8aa2hin34m332fks.html";
+        console.log("success")
+    }
+    else if(document.getElementById("username").value == "ansh2024@szoftvertelemechanics.com" && document.getElementById("password").value == "An@27655"){
+    window.location.href = "an2h37dh29fs72h3.html";
+    }
+}
+
+
+
+function getback(){
+    window.location.href = "index.html";
+}
