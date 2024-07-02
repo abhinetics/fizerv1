3f0ac8fe0f07aa7a268d626f5a16579d2cde1371
abhijeetkumarsinghofficial <hackker4200@gmail.com>
2024-07-02 10:46:51 +0530

    filees

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function Butclicked(){
     console.log("clicked")
-    if(document.getElementById("username").value == "abhijeet2024@szoftvertelemechanics.com" && document.getElementById("password").value == "Ab@32234"){
+    if(document.getElementById("username").value == "abhijeet.270@szoftvertelemechanics.com" && document.getElementById("password").value == "Abhijeet@270"){
         window.location.href = "c6sj39db2sk24.html";
         console.log("success")
     }
-    else if(document.getElementById("username").value == "faishal2024@szoftvertelemechanics.com" && document.getElementById("password").value == "Fa@54677"){
+    else if(document.getElementById("username").value == "Faishal.267@szoftvertelemechanics.com" && document.getElementById("password").value == "Faishal@267"){
         window.location.href = "f8aa2hin34m332fks.html";
         console.log("success")
     }
-    else if(document.getElementById("username").value == "ansh2024@szoftvertelemechanics.com" && document.getElementById("password").value == "An@27655"){
+    else if(document.getElementById("username").value == "Ansh.265@szoftvertelemechanics.com" && document.getElementById("password").value == "Ansh@265"){
     window.location.href = "an2h37dh29fs72h3.html";
     }
 }
